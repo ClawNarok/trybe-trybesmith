1275d00f9250c6645341802dde15853dbedaca4c
ClawNarok <arthurvgf@gmail.com>
2022-08-08 18:53:37 -0300

    teste remoto

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -11,11 +11,11 @@ const errorHandlerMiddleware = (
   res: Response,
   _next: NextFunction,
 ) => {
-  if (!err.message.includes('required') && !err.message.includes('invalid')) {
-    return res.status(errors.UnprocessableEntity).json({ message: err.message });
-  }
+  // if (!err.message.includes('required') && !err.message.includes('invalid')) {
+  //   return res.status(errors.UnprocessableEntity).json({ message: err.message });
+  // }
   const status = errors[err.name];
-  if (!status) return res.sendStatus(500);
+  if (!status) return res.status(500).json({ message: err.message });
   res.status(status).json({ message: err.message });
 };
 
