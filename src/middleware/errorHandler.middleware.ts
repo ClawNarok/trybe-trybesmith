6f5bf5b0669eb20552812be9420ca7855296554c
ClawNarok <arthurvgf@gmail.com>
2022-08-11 17:10:45 -0300

    retirando comentários de teste em monitoria

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -11,11 +11,9 @@ const errorHandlerMiddleware = (
   res: Response,
   _next: NextFunction,
 ) => {
-  console.log('|0000000000000000000|', err.message);
-  
-  // if (!err.message.includes('required') && !err.message.includes('invalid')) {
-  //   return res.status(errors.UnprocessableEntity).json({ message: err.message });
-  // }
+  if (!err.message.includes('required') && !err.message.includes('invalid')) {
+    return res.status(errors.UnprocessableEntity).json({ message: err.message });
+  }
   const status = errors[err.name];
   if (!status) return res.status(500).json({ message: err.message });
   res.status(status).json({ message: err.message });
